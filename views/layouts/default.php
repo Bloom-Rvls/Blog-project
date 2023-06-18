f43c8ccf70aeb91e386dc640cf696ae731359a13
Bloom rvls <angelicaravelonjohanison@gmail.com>
2023-06-18 16:19:00 +0300

    add footer section

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,15 +7,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title><?= $title ?? 'Mon site' ?></title>
 </head>
-<body>
+<body class="d-flex flex-column h-100">
     
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a href="" class="navbar-brand">Mon site</a>
+        <div class="container">
+            <a href="" class="navbar-brand">Mon site</a>
+        </div>
+        
     </nav>
 
     <div class="container mt-4">
         <?= $content ?>
     </div>
+
+    <footer class="bg-light py-4 footer mt-auto">
+        <div class="container">
+            <?php if(defined('DEBUG_TIME')): ?>
+            page générée en <?= round(1000 * (microtime(true) - DEBUG_TIME)) ?> ms
+            <?php endif ?>
+        </div>
+    </footer>
 
 </body>
 </html>
