12d6047b636a1e7537c370df492b1aff598ad883
Bloom rvls <angelicaravelonjohanison@gmail.com>
2023-06-29 11:11:56 +0300

    create pagiantion system

--- views/post/index.php
+++ views/post/index.php
@@ -8,7 +8,31 @@ use App\Model\Post;
  $pdo = new PDO('mysql:host=localhost:3306;dbname=blog;charset=utf8', 'root', '', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
  ]);
- $query = $pdo->query('SELECT * FROM post ORDER BY created_at DESC LIMIT 12');
+
+ $page = $_GET['page'] ?? 1;
+
+ if (!filter_var($page, FILTER_VALIDATE_INT)) {
+    throw new Exception('Numero de page invalide');
+ }
+
+ /*if ($page = '1') {
+    header('Location: ' . $router->url('home'));
+    http_response_code(301);
+    exit();
+ }*/
+
+ $currentPage = (int)$page;
+ if($currentPage <= 0) {
+    throw new Exception('Numero de page invalide');
+ }
+ $count = (int)$pdo->query('SELECT count(id) FROM post')->fetch(PDO::FETCH_NUM)[0];
+ $perPage = 12;
+ $pages = ceil($count / $perPage);
+ if($currentPage > $pages) {
+    throw new Exception('Cette page n\'existe pas');
+ }
+ $offset = $perPage * ($currentPage - 1);
+ $query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT $perPage OFFSET $offset");
  $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
  ?>
 
@@ -20,6 +44,19 @@ use App\Model\Post;
             <?php require 'card.php'; ?>
         </div>
     <?php endforeach; ?>
+</div>
+
+<div class="d-flex justify-content-between my-4">
+    <?php if($currentPage > 1): ?>
+        <?php
+        $link = $router->url('home');
+        if($currentPage > 2) $link .= '?page=' . ($currentPage - 1);
+        ?>
+        <a href="<?= $link?>" class="btn btn-primary">&laquo; Page précédente</a>
+    <?php endif; ?>
+    <?php if($currentPage < $pages): ?>
+        <a href="<?= $router->url('home')?>?page=<?= $currentPage + 1 ?>" class="btn btn-primary ml-auto">Page suivante &raquo;</a>
+    <?php endif; ?>
 </div>
 
 
